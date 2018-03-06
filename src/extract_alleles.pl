36acb33e99d95af040cc9eccb48e9c626e6201f9
chenh1gis <chenh1@gis.a-star.edu.sg>
2018-03-06 11:03:59 +0800

    Add files via upload

---
+++ src/extract_alleles.pl
@@ -0,0 +1,147 @@
+#!/usr/bin/perl
+
+##########################################################################
+## Author: Hui Chen
+## Created Time: 2017-8-17 09:40:44
+## File Name: extract_allele.pl
+##########################################################################
+#use strict;
+#use warnings;
+
+$SUBTYPE=$ARGV[0];
+$FILE=$ARGV[1];
+$PATH=$ARGV[2];
+$COMBINED=$ARGV[3];
+$ALIGNED=$ARGV[4];
+$OUTFILE=$ARGV[5];
+
+
+if ($SUBTYPE==1)
+{
+	@CODON=(89,97,129,134,161,185,186,221,222,226);
+}
+elsif ($SUBTYPE==2)
+{
+	@CODON=(21,127,129,183,190,191,222,223,225);
+}
+elsif ($SUBTYPE==3)
+{
+	@CODON=(138,145,156,158,159,160,183,186,190,193,194,219,226,246);
+}
+else
+{
+	print "Please re-run this script and input the correct parameter of different influenza subtypes:\n1:H1N1pdm\n2:H1N1seasonal\n3:H3N2\n";
+	exit(0);
+}
+
+%Genetic_code=('TCA'=>'S','TCC'=>'S','TCG'=>'S','TCT'=>'S','TTC'=>'F','TTT'=>'F','TTA'=>'L','TTG'=>'L','TAC'=>'Y','TAT'=>'Y','TAA'=>'-','TAG'=>'-','TGC'=>'C','TGT'=>'C','TGA'=>'-','TGG'=>'W','CTA'=>'L','CTC'=>'L','CTG'=>'L','CTT'=>'L','CCA'=>'P','CCC'=>'P','CCG'=>'P','CCT'=>'P','CAC'=>'H','CAT'=>'H','CAA'=>'Q','CAG'=>'Q','CGA'=>'R','CGC'=>'R','CGG'=>'R','CGT'=>'R','ATA'=>'I','ATC'=>'I','ATT'=>'I','ATG'=>'M','ACA'=>'T','ACC'=>'T','ACG'=>'T','ACT'=>'T','AAC'=>'N','AAT'=>'N','AAA'=>'K','AAG'=>'K','AGC'=>'S','AGT'=>'S','AGA'=>'R','AGG'=>'R','GTA'=>'V','GTC'=>'V','GTG'=>'V','GTT'=>'V','GCA'=>'A','GCC'=>'A','GCG'=>'A','GCT'=>'A','GAC'=>'D','GAT'=>'D','GAA'=>'E','GAG'=>'E','GGA'=>'G','GGC'=>'G','GGG'=>'G','GGT'=>'G');
+
+if ($SUBTYPE==1)
+{
+	$filename="../data/H1N1seasonal/H1N1seasonal_HA1_sequence_ref.fa";
+	if (-e $filename)
+	{
+		open (REF,"$filename");
+	}
+	else
+	{
+		print ("ERROR: the reference file \"H1N1seasonal_HA1_sequence_ref.fa\" is not under directory \"../data/H1N1seasonal\".");
+		exit(0);	
+	}
+}
+elsif ($SUBTYPE==2)
+{
+        $filename="../data/H1N1pdm/H1N1pdm_HA1_sequence_ref.fa";
+        if (-e $filename)
+        {
+                open (REF,"$filename");
+        }
+        else
+        {
+                print ("ERROR: the reference file \"H1N1pdm_HA1_sequence_ref.fa\" is not under current directory \"../data/H1N1pdm\".");
+                exit(0);
+        }
+}
+else
+{
+        $filename="../data/H3N2/H3N2_HA1_sequence_ref.fa";
+        if (-e $filename)
+        {
+                open (REF,"$filename");
+        }
+        else
+        {
+                print ("ERROR: the reference file \"H3N2_HA1_sequence_ref.fa\" is not under current directory \"../data/H3N2\".");
+                exit(0);
+        }
+}
+
+open (FILE,"$FILE");
+$title=<FILE>;
+chomp $title;
+$seq=<FILE>;
+chomp $seq;
+close FILE;
+open (COMBINED,">$COMBINED");
+print COMBINED "$title\n$seq\n";
+$title_ref=<REF>;
+$seq_ref=<REF>;
+print COMBINED $title_ref;
+print COMBINED $seq_ref; 
+close COMBINED;
+
+system("$PATH -in $COMBINED -out $ALIGNED");
+print "\nSequence alignment has finished!\n";
+
+$seq=$seq_ref="";
+open (ALIGN,"$ALIGNED");
+$title=<ALIGN>;
+chomp $title;
+$line=<ALIGN>;
+chomp $line;
+while ($line!~/\>/)
+{
+	$seq=$seq.$line;
+	$line=<ALIGN>;
+	chomp $line;
+}
+$title_ref=$line;
+while ($line=<ALIGN>)
+{
+	chomp $line;
+	$seq_ref=$seq_ref.$line;
+}
+$len=length($seq_ref);
+for ($index=0;$index<$len;$index++)
+{
+	$string=substr($seq_ref,0,$index+1);
+	$string1=$string;
+	$string1=~s/-//g;
+	$length=length($string1);
+	$INDEX{$length-1}=$index;
+}
+close ALIGN;
+
+foreach $codon(@CODON)
+{
+	$char1=substr($seq,$INDEX{($codon-1)*3},1);
+	$char2=substr($seq,$INDEX{($codon-1)*3+1},1);
+	$char3=substr($seq,$INDEX{($codon-1)*3+2},1);
+	if ($char1 eq '-' || $char2 eq '-' || $char3 eq '-')
+	{
+        	print "ERROR: the sequence of H3 HA1 is not complete and information of some codons are missing!\n";
+	        exit(0);
+	}
+	else
+	{
+		$CHAR{$codon}=$char1.$char2.$char3;
+	}
+}
+
+open (ALLE,">$OUTFILE");
+print ALLE "CODON\tAminoAcid\n";
+foreach $codon(@CODON)	
+{
+	print ALLE "$codon\t$Genetic_code{$CHAR{$codon}}\n";
+}
+close ALLE;
---
+++ src/generate_report.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/perl
+
+#############################################################################
+## Author: Hui Chen
+## Created Time: 2018-1-3 14:41:44
+## File Name: Generate_report.pl
+## Description: MADE is deposited in https://github.com/chenh1gis/MADE_docker
+#############################################################################
+
+##use strict;
+##use warnings;
+use Getopt::Long;
+
+$SUBTYPE="";	## H1N1seasonal:1 H1N1pdm:2 H3N2:3
+$ALLE_FILE="";	## ALLE:1 SEQ:0
+$ID="NA";
+$STRAIN="NA";
+$HOST="NA";
+$PASSAGE="NA";
+$FILE="";
+
+GetOptions(
+        'subtype=s' => \$SUBTYPE,
+        'is_allelic_file=s' => \$ALLE_FILE,
+        'id=s' => \$ID,
+	'strain=s' => \$STRAIN,
+	'host=s' => \$HOST,
+	'passage=s' => \$PASSAGE,
+	'input_file=s' => \$FILE 
+);
+
+if (!-e "$FILE"){print "Error : file \"$FILE\" does not exist!\n"; exit(0);}
+
+%Genetic_code=('TCA'=>'S','TCC'=>'S','TCG'=>'S','TCT'=>'S','TTC'=>'F','TTT'=>'F','TTA'=>'L','TTG'=>'L','TAC'=>'Y','TAT'=>'Y','TAA'=>'-','TAG'=>'-','TGC'=>'C','TGT'=>'C','TGA'=>'-','TGG'=>'W','CTA'=>'L','CTC'=>'L','CTG'=>'L','CTT'=>'L','CCA'=>'P','CCC'=>'P','CCG'=>'P','CCT'=>'P','CAC'=>'H','CAT'=>'H','CAA'=>'Q','CAG'=>'Q','CGA'=>'R','CGC'=>'R','CGG'=>'R','CGT'=>'R','ATA'=>'I','ATC'=>'I','ATT'=>'I','ATG'=>'M','ACA'=>'T','ACC'=>'T','ACG'=>'T','ACT'=>'T','AAC'=>'N','AAT'=>'N','AAA'=>'K','AAG'=>'K','AGC'=>'S','AGT'=>'S','AGA'=>'R','AGG'=>'R','GTA'=>'V','GTC'=>'V','GTG'=>'V','GTT'=>'V','GCA'=>'A','GCC'=>'A','GCG'=>'A','GCT'=>'A','GAC'=>'D','GAT'=>'D','GAA'=>'E','GAG'=>'E','GGA'=>'G','GGC'=>'G','GGG'=>'G','GGT'=>'G');
+
+%SYMBOL=('t'=>'T','c'=>'C','a'=>'A','g'=>'G');
+
+@array=split(/@/,$EMAIL);
+undef %FILE;
+opendir(DIR,"./");
+@file_list=readdir(DIR);
+foreach $file(@file_list)
+{
+        $FILE{$file}=1;
+}
+closedir (DIR);
+
+if ($ALLE_FILE==0)
+{
+	if ($FILE!~/\.fa/) {print "Error : please specify the correct nucleotide sequence file in FASTA format!\n"; exit(0);}
+	system ("cp $FILE file_sequence.fa");
+}
+elsif ($ALLE_FILE==1)
+{
+        if ($FILE!~/\.txt/) {print "Error : please specify the correct allelic file in TXT format!\n"; exit(0);}
+	system ("cp $FILE file_allele.txt");
+}
+else
+{
+	print "Error : please specify the correct type of input file!\n";
+	exit(0);
+}
+
+if ($SUBTYPE!=1 && $SUBTYPE!=2 && $SUBTYPE!=3)
+{
+	print "Error : please specify the correct virus subtype!\n";
+	exit(0);
+}
+
+if ($ALLE_FILE==0)
+{
+	open (SEQUENCE,"file_sequence.fa");
+	$title=<SEQUENCE>;
+	chomp $title;
+	$seq=<SEQUENCE>;
+	chomp $seq;
+	if ($title!~/>/)
+	{
+        	print "ERROR: the sequence title is in wrong format, please update the sequence file in FASTA format\n"; exit(0);
+	}
+	if ($seq!~/[^ATCGatcg]/)
+	{
+		$seq=~s/a/A/g;
+		$seq=~s/t/T/g;
+		$seq=~s/c/C/g;
+		$seq=~s/g/G/g;
+		$aa=$AA=$SEQ="";
+	        $len=length($seq);
+		for ($i=0;$i<$len/3;$i++)
+		{
+		        $base=substr($seq,$i*3,1);
+	                $base1=substr($seq,$i*3+1,1);
+	                $base2=substr($seq,$i*3+2,1);
+	                $BASE=$base.$base1.$base2;
+                        $aa=$aa.$Genetic_code{$BASE};
+		}
+	}
+	else
+	{
+		print "Error : ambiguous character is not  acceptable for MADE analysis!\n";
+		exit(0);	
+	}
+	close SEQUENCE;
+}
+
+if ($SUBTYPE==1)
+{
+	$DEFINITION="influenza A H1N1 seasonal virus HA segment";
+        if ($ALLE_FILE==0)
+        {
+                system ("perl extract_alleles.pl 1 file_sequence.fa ../muscle/muscle3.8.31_i86linux64 sequence_combined.fa sequence_combined.afa file_allele.txt");
+                system ("rm sequence_combined.fa; rm sequence_combined.afa");
+        }	
+        system ("R -e \"install.packages('prettydoc'); library(prettydoc); rmarkdown::render(\'MADE_H1N1seasonal.Rmd\',html_pretty(),output_dir='./',params=list(id=\'$ID\',def=\'$DEFINITION\',strain=\'$STRAIN\',host=\'$HOST\',pass=\'$PASSAGE\'))\"");
+	print "Analysis finished!\nPlease refer to \"MADE_H1N1seasonal.html\" under \.\/src\n";
+}elsif ($SUBTYPE==2)
+{
+	$DEFINITION="influenza A H1N1 pandemic virus HA segment";
+        if ($ALLE_FILE==0)
+        {
+                system ("perl extract_alleles.pl 2 file_sequence.fa ../muscle/muscle3.8.31_i86linux64 sequence_combined.fa sequence_combined.afa file_allele.txt");
+                system ("rm sequence_combined.fa; rm sequence_combined.afa");
+        }	
+        system ("R -e \"install.packages('prettydoc'); library(prettydoc); rmarkdown::render(\'MADE_H1N1pdm.Rmd\',html_pretty(),output_dir='./',params=list(id=\'$ID\',def=\'$DEFINITION\',strain=\'$STRAIN\',host=\'$HOST\',pass=\'$PASSAGE\'))\"");
+	print "Analysis finished!\nPlease refer to \"MADE_H1N1pdm.html\" under \.\/src\n";
+}else
+{
+	$DEFINITION="influenza A H3N2 virus HA segment";
+        if ($ALLE_FILE==0)
+	{
+                system ("perl extract_alleles.pl 3 file_sequence.fa ../muscle/muscle3.8.31_i86linux64 sequence_combined.fa sequence_combined.afa file_allele.txt");
+#		system ("rm sequence_combined.fa; rm sequence_combined.afa");
+        }
+	system ("R -e \"install.packages('prettydoc'); library(prettydoc); rmarkdown::render(\'MADE_H3N2.Rmd\',html_pretty(),output_dir='./',params=list(id=\'$ID\',def=\'$DEFINITION\',strain=\'$STRAIN\',host=\'$HOST\',pass=\'$PASSAGE\'))\"");
+	print "Analysis finished!\nPlease refer to \"MADE_H3N2.html\" under \.\/src\n";
+}
